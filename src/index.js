71e4b187b76dcf6c50e21aa392c8c900641d7d24
anastasiia.dashko <anastasiia.dashko@student.tuke.sk>
2022-05-21 12:47:37 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './global.css';
+import { StateContextProvider } from './context/StateContextProvider'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(
+    <StateContextProvider>
+        <Router>
+            <App />
+        </Router>
+    </StateContextProvider>,
+    document.getElementById('root'),
+    );
